8df2adcff830eb1a0e4de28c5c6c9281b90a4d9d
veronicasih <veronicasih18@gmail.com>
2022-03-18 13:55:33 -0400

    fixing spotify authentication for deployment

--- app/spotify.py
+++ app/spotify.py
@@ -4,6 +4,7 @@ from app.config import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIPY_REDIREC
 
 def connect_spotify():
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope=SPOTIPY_SCOPE, cache_path = SPOTIPY_OAUTH_CACHE))
+    print(sp)
     return get_token(sp)
 
 def get_token(sp):
