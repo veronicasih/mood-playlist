5f34edb4b954001f5f3b9dc3d06bc20db5b1a75c
veronicasih <veronicasih18@gmail.com>
2022-03-18 11:43:56 -0400

    testing out why GET / doesn't work when deploying to heroku pt 2

--- app/__init__.py
+++ app/__init__.py
@@ -7,17 +7,18 @@ def create_app():
     app = Flask('app')
     app.config.from_pyfile('config.py', silent = True)
 
-    # @app.route('/', methods = ['GET', 'POST'])
-    # def index(): 
-    #     spotipy_object = spotify.connect_spotify()
-    #     available_genres = spotify.get_available_genres(spotipy_object)
-    #     playlist_recs = None 
-    #     if request.method == 'POST':
-    #         print('POST request received', file = sys.stdout)
-    #         selectedGenres = request.get_json()['selectedGenres']    
-    #         playlist_recs = spotify.get_recommendations(spotipy_object, selectedGenres, available_genres) # this returns a list of dictionaries where each dictionay corresponds to a recommended track
-    #         return render_template('recommendations.html', genres = available_genres, themes = app.config['THEMES'], recommended_tracks = playlist_recs)
-    #     else: 
-    #         return render_template('index.html', genres = available_genres, themes = app.config['THEMES'])
+    @app.route('/', methods = ['GET', 'POST'])
+    def index(): 
+        return "hello"
+        # spotipy_object = spotify.connect_spotify()
+        # available_genres = spotify.get_available_genres(spotipy_object)
+        # playlist_recs = None 
+        # if request.method == 'POST':
+        #     print('POST request received', file = sys.stdout)
+        #     selectedGenres = request.get_json()['selectedGenres']    
+        #     playlist_recs = spotify.get_recommendations(spotipy_object, selectedGenres, available_genres) # this returns a list of dictionaries where each dictionay corresponds to a recommended track
+        #     return render_template('recommendations.html', genres = available_genres, themes = app.config['THEMES'], recommended_tracks = playlist_recs)
+        # else: 
+        #     return render_template('index.html', genres = available_genres, themes = app.config['THEMES'])
             
     return app
